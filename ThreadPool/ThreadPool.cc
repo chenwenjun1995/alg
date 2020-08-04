f4620cb376f4bafca29c81e81d5c3e35ebe9cf4a
chenwenjun1995 <2512831937@qq.com>
2020-08-04 19:59:02 +0800

    0804

--- ThreadPool/ThreadPool.cc
+++ ThreadPool/ThreadPool.cc
@@ -1,3 +1,11 @@
-#include "threadpool.h"
+#include "ThreadPool.h"
 
-void 
+ThreadPool::~ThreadPool() {
+
+}
+
+void ThreadPool::start(int thread_num, std::function<void> func) {
+    for (int i = 0; i < thread_num; ++i) {
+        thread_vec_.emplace_back(std::move(std::thread(func)));
+    }
+}
--- ThreadPool/ThreadPool.h
+++ ThreadPool/ThreadPool.h
@@ -7,8 +7,7 @@ public:
     ThreadPool(){}
     ~ThreadPool(){}
 
-    void start(int thread_num);
-    void add_task(const std::function<void>& func);
+    void start(int thread_num, const std::function<void>& callback);
 private:
     std::vector<std::thread> thread_vec_;
 }
