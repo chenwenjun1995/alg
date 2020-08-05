2c9bcbbbaa3fdee74851d428f3b0e341bf84a88e
chenwenjun <2512831937@qq.com>
2020-08-05 23:06:07 +0800

    080502

--- ThreadPool/ThreadPool.cc
+++ ThreadPool/ThreadPool.cc
@@ -13,25 +13,27 @@ ThreadPool::~ThreadPool()
 void ThreadPool::start(int thread_num) 
 {
     for (int i = 0; i < thread_num; ++i) {
-        thread_vec_.push_back(new std::thread(std::bind(&ThreadPool::run, this)));
+        thread_vec_.push_back(new std::thread(&ThreadPool::run, this));
     }
 }
 
 void ThreadPool::run() 
 {
-    int task;
-    {
-        std::lock_guard<std::mutex>  lg(mutex_);
-        if (!data_list_.empty()) {
-            int task = data_list_.front();
-            data_list_.pop_front();
-            std::cout<< std::this_thread::get_id() << ":" <<task << std::endl;
+    while (true) {
+        {
+            if (!data_list_.empty()) {
+                int task;
+                {
+                    std::lock_guard<std::mutex>  lg(mutex_);
+                    task = data_list_.front();
+                    data_list_.pop_front();
+                }
+                std::cout<< "thread[" << std::this_thread::get_id() << "]:" << task << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
         }
-        else {
-            std::this_thread::sleep_for (std::chrono::seconds(1));
-        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
-    std::this_thread::sleep_for (std::chrono::seconds(30));
 }
 
 void ThreadPool::add_task(int task) 
--- test/threadpool.cc
+++ test/threadpool.cc
@@ -4,10 +4,11 @@ int main()
 {
     ThreadPool tp;
     tp.start(10);
-    for (int i = 0; i < 9999999999; i++) {
+    for (int i = 0; i < 60; i++) {
         tp.add_task(i);
         std::cout<< "task:" << i << std::endl;
-        std::this_thread::sleep_for (std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
+    std::this_thread::sleep_for(std::chrono::seconds(600));
     return 0;
 }
